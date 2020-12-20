5e825b17aaf8dd242f5e7962ba4a13419af160b3
codebrain001 <abozejohn@gmail.com>
2020-12-20 22:43:19 +0100

    Fixed the navigation bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,35 @@
 // Mobile navigation toggle button
 let nav_interative = () => {
-let burger = document.querySelector('.burger');
-let nav = document.querySelector('.menu');
-let navBar = document.querySelector('nav');
-let menuLinks = document.querySelectorAll('.menu li');
+    let burger = document.querySelector('.burger');
+    let nav = document.querySelector('.menu');
+    let navBar = document.querySelector('nav');
+    let menuLinks = document.querySelectorAll('.menu li');
 
-// Toggle Menu
-burger.addEventListener('click', () => {
-    nav.classList.toggle('menu-active');
+    // Toggle Menu
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('menu-active');
 
-    // Fixed the body section 
-    document.querySelector('body').classList.toggle('body-fixed');
-   
-    // Remove the blur on the navigation bar (Mobile)
-    navBar.classList.toggle('remove-blur');
-
-    // Add the blur on the navigation bar (Mobile)
-    nav.classList.toggle('add-blur');
-
-    // Animate Links
-    menuLinks.forEach((link, index)=>{
-        if (link.style.animation){
-            link.style.animation = '';
-        } else {
-            link.style.animation = `linkFade 0.5s ease forwards ${index/7 +2}s`;
-        }
-        });
-        // Burger toggle
-        burger.classList.toggle('toggle');
-
+        // Fixed the body section 
+        document.querySelector('body').classList.toggle('body-fixed');
     
-})
+        // Remove the blur on the navigation bar (Mobile)
+        navBar.classList.toggle('remove-blur');
+
+        // Add the blur on the navigation bar (Mobile)
+        nav.classList.toggle('add-blur');
+
+        // Animate Links
+        menuLinks.forEach((link, index)=>{
+            if (link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `linkFade 0.5s ease forwards ${index/7 +2}s`;
+            }
+            });
+
+            // Burger toggle
+            burger.classList.toggle('toggle');
+    })
 
 }
 
